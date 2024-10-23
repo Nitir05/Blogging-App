3caba944a546f943604ecc69117545a7a52eb604
Nitir05 <nrahangdal86@gmail.com>
2024-10-23 23:24:21 +0530

    Page and route added to show each blog

--- routes/blog.js
+++ routes/blog.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const path = require("path");
 const multer = require("multer");
 const Blog = require("../models/blog");
+const Comment = require("../models/comment");
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -35,6 +36,34 @@ router.post("/", upload.single("coverImage"), async (req, res) => {
         createdBy: req.user._id,
     });
     return res.redirect(`/blog/${blog._id}`);
+});
+
+router.get("/:id", async (req, res) => {
+    const blogId = req.params.id;
+
+    const blog = await Blog.findById(blogId).populate("createdBy");
+    const comments = await Comment.find({ blogId: blogId }).populate(
+        "createdBy"
+    );
+
+    return res.render("blog", {
+        user: req.user,
+        blog,
+        comments,
+    });
+});
+
+router.post("/comment/:blogId", async (req, res) => {
+    const { content } = req.body;
+    const blogId = req.params.blogId;
+
+    Comment.create({
+        blogId,
+        content,
+        createdBy: req.user._id,
+    });
+
+    return res.redirect(`/blog/${blogId}`);
 });
 
 module.exports = router;
